ecffe4d58e07929fc4b4ce58bbc718a6f0bf3e65
Dennis Wenrich <dennisfinley@gmail.com>
2022-04-10 10:04:39 -0400

    added a check to see if questions were answered

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="ctr">
-    <newQuestions />
-    <newResult />
+    <newQuestions v-if="questionsAnswered < questions.length"/>
+    <newResult v-else/>
     <button type="button" class="reset-btn">Reset</button>
   </div>
 </template>
@@ -18,6 +18,7 @@ export default {
   },
   data () {
     return {
+      questionsAnswered: 0,
       questions: [
         {
           q: 'What is 2 + 2?',
